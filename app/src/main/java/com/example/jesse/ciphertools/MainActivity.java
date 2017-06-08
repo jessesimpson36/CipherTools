14c6c8db82ef6629517e011bfc7ce2608c4443a6
jessesimpson36 <jesse.simpson36@gmail.com>
2017-06-08 00:04:12 -0400

    created a share button

--- app/src/main/java/com/example/jesse/ciphertools/MainActivity.java
+++ app/src/main/java/com/example/jesse/ciphertools/MainActivity.java
@@ -1,29 +1,27 @@
 package com.example.jesse.ciphertools;
 
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private Menu savedMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +38,8 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
     }
 
     @Override
@@ -55,7 +55,8 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.share_main, menu);
+        savedMenu = menu;
         return true;
     }
 
@@ -66,10 +67,6 @@ public class MainActivity extends AppCompatActivity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
@@ -80,9 +77,38 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.atbash) {
+        // makes the texts invisible unless its necessary.
+        if (id != R.id.caesarian_shift){
 
-            setContentView(R.layout.activity_main);
+            EditText output = (EditText) findViewById(R.id.output);
+            output.setVisibility(View.VISIBLE);
+
+            TextView caesar_5 = (TextView) findViewById(R.id.caesarText_5);
+            TextView caesar_4 = (TextView) findViewById(R.id.caesarText_4);
+            TextView caesar_3 = (TextView) findViewById(R.id.caesarText_3);
+            TextView caesar_2 = (TextView) findViewById(R.id.caesarText_2);
+            TextView caesar_1 = (TextView) findViewById(R.id.caesarText_1);
+            TextView caesar0 = (TextView) findViewById(R.id.caesarText0);
+            TextView caesar1 = (TextView) findViewById(R.id.caesarText1);
+            TextView caesar2 = (TextView) findViewById(R.id.caesarText2);
+            TextView caesar3 = (TextView) findViewById(R.id.caesarText3);
+            TextView caesar4 = (TextView) findViewById(R.id.caesarText4);
+            TextView caesar5 = (TextView) findViewById(R.id.caesarText5);
+
+            caesar_5.setText("");
+            caesar_4.setText("");
+            caesar_3.setText("");
+            caesar_2.setText("");
+            caesar_1.setText("");
+            caesar0.setText("");
+            caesar1.setText("");
+            caesar2.setText("");
+            caesar3.setText("");
+            caesar4.setText("");
+            caesar5.setText("");
+        }
+
+        if (id == R.id.atbash) {
 
             EditText input = (EditText) findViewById(R.id.input);
             EditText output = (EditText) findViewById(R.id.output);
@@ -109,25 +135,24 @@ public class MainActivity extends AppCompatActivity
 
                     output.setText(BasicCiphers.translateAtbash(inputString));
 
+                    updateShare();
                 }
 
             });
 
 
         } else if (id == R.id.caesarian_shift) {
-            LinearLayout mainLayout = (LinearLayout) findViewById(R.id.);
-            LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            View layout = inflater.inflate(R.layout.caesarian_layout, null);
-            mainLayout.removeAllViews();
-            mainLayout.addView(layout);
 
-            EditText input = (EditText) findViewById(R.id.caesarInput);
+
+            EditText input = (EditText) findViewById(R.id.input);
+            EditText output = (EditText) findViewById(R.id.output);
+            output.setVisibility(View.INVISIBLE);
 
 
             // this is where the text will be translated.
             input.addTextChangedListener(new TextWatcher() {
 
-                EditText input = (EditText) findViewById(R.id.caesarian_shift);
+                EditText input = (EditText) findViewById(R.id.input);
 
                 @Override
                 public void afterTextChanged(Editable s) {}
@@ -155,17 +180,18 @@ public class MainActivity extends AppCompatActivity
                     TextView caesar4 = (TextView) findViewById(R.id.caesarText4);
                     TextView caesar5 = (TextView) findViewById(R.id.caesarText5);
 
-                    caesar_5.setText(BasicCiphers.caesarianShift(-5, inputString ));
-                    caesar_4.setText(BasicCiphers.caesarianShift(-4, inputString ));
-                    caesar_3.setText(BasicCiphers.caesarianShift(-3, inputString ));
-                    caesar_2.setText(BasicCiphers.caesarianShift(-2, inputString ));
-                    caesar_1.setText(BasicCiphers.caesarianShift(-1, inputString ));
-                    caesar0.setText(BasicCiphers.caesarianShift(0, inputString ));
-                    caesar1.setText(BasicCiphers.caesarianShift(1, inputString ));
-                    caesar2.setText(BasicCiphers.caesarianShift(2, inputString ));
-                    caesar3.setText(BasicCiphers.caesarianShift(3, inputString ));
-                    caesar4.setText(BasicCiphers.caesarianShift(4, inputString ));
-                    caesar5.setText(BasicCiphers.caesarianShift(5, inputString ));
+                    caesar_5.setText("Shifted Left 5 Times: " + BasicCiphers.caesarianShift(-5, inputString ));
+                    caesar_4.setText("Shifted Left 4 Times: " + BasicCiphers.caesarianShift(-4, inputString ));
+                    caesar_3.setText("Shifted Left 3 Times: " + BasicCiphers.caesarianShift(-3, inputString ));
+                    caesar_2.setText("Shifted Left 2 Times: " + BasicCiphers.caesarianShift(-2, inputString ));
+                    caesar_1.setText("Shifted Left 1 Time: " + BasicCiphers.caesarianShift(-1, inputString ));
+                    caesar0.setText("Original Message: " + BasicCiphers.caesarianShift(0, inputString ));
+                    caesar1.setText("Shifted Right 1 Time: " + BasicCiphers.caesarianShift(1, inputString ));
+                    caesar2.setText("Shifted Right 2 Times: " + BasicCiphers.caesarianShift(2, inputString ));
+                    caesar3.setText("Shifted Right 3 Times: " + BasicCiphers.caesarianShift(3, inputString ));
+                    caesar4.setText("Shifted Right 4 Times: " + BasicCiphers.caesarianShift(4, inputString ));
+                    caesar5.setText("Shifted Right 5 Times: " + BasicCiphers.caesarianShift(5, inputString ));
+
 
                 }
 
@@ -181,4 +207,21 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    private void updateShare(){
+        MenuItem item = savedMenu.findItem(R.id.shareButton);
+        EditText output = (EditText) findViewById(R.id.output);
+        ShareActionProvider mShareActionProvider = (ShareActionProvider)MenuItemCompat.getActionProvider(item);
+        //create the sharing intent
+        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+        sharingIntent.setType("text/plain");
+        String shareBody = output.getText().toString();
+        sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Translate This!");
+        sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+
+        //then set the sharingIntent
+        mShareActionProvider.setShareIntent(sharingIntent);
+    }
+
+
 }
